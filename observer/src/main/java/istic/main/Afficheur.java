30284f64d000f2a68a0b80848e24e850ba485f12
Unknown <ledoux.simon@gmail.com>
2018-01-13 17:35:46 +0100

    Add GUI javafx

--- observer/src/main/java/istic/main/Afficheur.java
+++ observer/src/main/java/istic/main/Afficheur.java
@@ -14,11 +14,43 @@ import istic.proxy.Canal;
 
 public class Afficheur implements ObserverGenerateurAsync {
 
-	@Override
-	public Future update() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	final static Logger logger = Logger.getLogger(Afficheur.class);
 
+    private Canal canal;
+    private Integer integer;
+    private Label value;
+
+    public Afficheur(Canal canal, Label value) {
+        this.canal = canal;
+        this.value = value;
+    }
+
+    public Afficheur(Canal canal) {
+        this.canal = canal;
+    }
+
+    public Future update() {
+        integer=canal.getValue();
+        logger.info("Afficheur [update] =>" + integer);
+        Platform.runLater(() -> value.setText(String.valueOf(integer % 100)));
+        return null;
+    }
+
+    public Label getValue() {
+        return value;
+    }
+
+    public void setValue(Label value) {
+        this.value = value;
+    }
+
+    public Canal getCanal() {
+        return canal;
+    }
+
+    public void setCanal(Canal canal) {
+        this.canal = canal;
+    }
 
 }
--- observer/src/main/java/istic/main/GuiCtrl.java
+++ observer/src/main/java/istic/main/GuiCtrl.java
@@ -28,9 +28,83 @@ public class GuiCtrl  implements Initializable {
 	final static Logger logger = Logger.getLogger(GuiCtrl.class);
 
 	
+    @FXML
+    private Label value1,value2,value3,value4;
+    
+    @FXML
+    private Label labelDesc;
+    
+    @FXML
+    private RadioButton radioSeq;
+    
+    @FXML
+    private RadioButton radioAtom;
+    
+    @FXML
+    private Button startStopBtn;
+    
+    final ToggleGroup toggleRadio = new ToggleGroup();
+
+    public static ScheduledExecutorService executorService;
+    
+    private Strategy myStrat;
+    
+    private Canal canal1,canal2,canal3,canal4;
+
+    private GenerateurImpl generator;
+
+    private boolean process=false;
+
+    @FXML
+    private void startStop(ActionEvent event) {
+    	
+        if(process){
+        	executorService.shutdown();
+        	process=false;
+        	logger.info("[STOP]");
+
+        } else {
+        	
+        	logger.info("[START]");
+        	process=true;
+        	executorService = new ScheduledThreadPoolExecutor(7);
+            generator = new GenerateurImpl();
+            
+            canal1 = new Canal(executorService, generator);
+            canal2 = new Canal(executorService, generator);
+            canal3 = new Canal(executorService, generator);
+            canal4 = new Canal(executorService, generator);
+            
+            if (radioAtom.isSelected()) {
+                myStrat = new Atomique(generator);
+            } else {
+              	myStrat = new Sequentielle(generator);
+            }
+          	
+            generator.add(canal1);
+            generator.add(canal2);
+            generator.add(canal3);
+            generator.add(canal4);
+
+            canal1.getDisplay().setValue(value1);
+            canal2.getDisplay().setValue(value2);
+            canal3.getDisplay().setValue(value3);
+            canal4.getDisplay().setValue(value4);
+
+            executorService.scheduleAtFixedRate(generator::createvalue, 0, 3000, TimeUnit.MILLISECONDS);
+            process=true;
+        }
+    }
 
     public void initialize(URL url, ResourceBundle rb) {
+    	
+    	labelDesc.setText("Welcome to AOC 2017 !\n\nSimon LEDOUX-LEVIN\nAlan MARZIN");
 
+    	
+        radioAtom.setToggleGroup(toggleRadio);
+        radioSeq.setToggleGroup(toggleRadio);
+
+        radioAtom.setSelected(true);
     }
 }
 
