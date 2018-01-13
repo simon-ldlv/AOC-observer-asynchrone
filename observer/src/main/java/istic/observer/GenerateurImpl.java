de1885d967ca06f7d39b8480b244828ea7982e53
Unknown <ledoux.simon@gmail.com>
2018-01-13 17:38:19 +0100

    Generateur action + observer update

--- observer/src/main/java/istic/observer/GenerateurImpl.java
+++ observer/src/main/java/istic/observer/GenerateurImpl.java
@@ -11,30 +11,64 @@ import istic.strategy.Strategy;
 public class GenerateurImpl implements GenerateurAsync {
 
 	final static Logger logger = Logger.getLogger(GenerateurImpl.class);
+	
+    private Integer value;
+    private List<ObserverGenerateurAsync> observersList;
 
-	@Override
-	public void add(ObserverGenerateurAsync o) {
-		// TODO Auto-generated method stub
-		
-	}
+    private Strategy strategy;
 
-	@Override
-	public void delete(ObserverGenerateurAsync o) {
-		// TODO Auto-generated method stub
-		
-	}
+    public GenerateurImpl() {
+        observersList=new ArrayList<>();
+    }
 
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		
-	}
+    public List<ObserverGenerateurAsync> getObservers() {
+        return observersList;
+    }
 
-	@Override
-	public Integer getValue() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public void setObservers(List<ObserverGenerateurAsync> observers) {
+        this.observersList = observers;
+    }
 
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Strategy getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    @Override
+    public Integer getValue() {
+        return value;
+    }
+
+    public void createvalue() {
+
+        this.value= (int)(Math.random() * 100);
+        logger.info("[createvalue] => "+value);
+        execute();
+    }
+
+
+    @Override
+    public void add(ObserverGenerateurAsync o) {
+        if(!observersList.contains(o)) {
+            observersList.add(o);
+        }
+    }
+
+    @Override
+    public void delete(ObserverGenerateurAsync o) {
+        observersList.remove(o);
+    }
+
+    @Override
+    public void execute() {
+        strategy.execute();
+    }
 
 }
